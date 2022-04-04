e7d50b6d78c1b84d9f0271843aae21b93b7cc425
Dyyndalski <szaur.kacper@gmail.com>
2022-04-04 09:59:22 +0200

    Initial commit

---
+++ src/main/java/pl/szaur/wheatherapp/WheatherAppApplication.java
@@ -0,0 +1,13 @@
+package pl.szaur.wheatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheatherAppApplication.class, args);
+    }
+
+}
